4157c0d41ac1646927439655b8b0a51e4b0d66e3
YefferSime <simehuaranccay@gmail.com>
2024-03-22 23:15:03 -0500

    laravel-render3

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel1212
+                    <img src="./descarga.png" alt="">
                 </div>
 
                 <div class="links">
